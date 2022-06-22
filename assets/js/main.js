2b82f50839b31d3ac8349f360a9ac2adb4bc0d6d
adam.etzion <adam.etzion@modivcare.com>
2022-06-22 16:51:52 -0600

    Update Index.js main.js and images

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".element").typed({
+        strings: ["^1000Hi there!^1000 I'm Adam.^500", "I'm a business guy turned engineer^1500", "Reach out and let's chat.^2000"],
+        typeSpeed: 0
+    });
+});
